e6c9adac784c8528cfe96b28521470beff2ac11a
Pratyush Kumar <pratyushkumar576@gmail.com>
2024-11-02 13:40:28 -0700

    update the document

--- src/main/java/com/kumar/lrucache/LRUCache.java
+++ src/main/java/com/kumar/lrucache/LRUCache.java
@@ -13,18 +13,36 @@ public abstract class LRUCache<K, V> {
         this.maxCapacity = maxCapacity;
     }
     
+    /**
+     * Get function returns the value stored against the key(if present in the cache).
+     * If element is not present in the cache, it returns -1.
+     * 
+     * @param key cache key
+     * @return value stored against the key
+     */
     public abstract V get(K key);
     public abstract void put(K key, V value);
 
+    /**
+     * Checks if cache is full.
+     * @return
+     */
     protected boolean isCacheFull() {
         return availableCache.size() >= maxCapacity ? true : false;
     }
 
+    /**
+     * Evicts the least recently used element.
+     */
     protected void evictLeastRecentlyElementFromCache() {
         K key = availableCache.removeFirst();
         cache.remove(key);
     }
 
+    /**
+     * Evects the input element
+     * @param key
+     */
     protected void evictLeastRecentlyElementFromCache(K key) {
         availableCache.remove(key);
         cache.remove(key);
